ef36451944e61e9049002c9e2310019810068f3f
Rukshan Dias <diasrukshan21@gmail.com>
2024-10-20 19:41:22 +0530

    My Angular fontend

--- angular-tour-of-heroes/src/app/app.module.ts
+++ angular-tour-of-heroes/src/app/app.module.ts
@@ -7,22 +7,32 @@ import { NavBarComponent } from './components/nav-bar/nav-bar.component';
 import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { ContuctUsComponent } from './components/contuct-us/contuct-us.component';
 import {RouterModule, Routes} from "@angular/router";
+import { AboutUsComponent } from './components/about-us/about-us.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import {MatTabsModule} from "@angular/material/tabs";
+import { AboutPageComponent } from './components/about-page/about-page.component';
 
 const routes :Routes =[
-  {path:'ContactUs', component:ContuctUsComponent}
+  {path:'ContactUs', component:ContuctUsComponent},
+  {path:'AboutUs',  component : AboutUsComponent },
+  {path:'AboutMe', component:AboutPageComponent}
 ]
 
 @NgModule({
   declarations: [
     AppComponent,
     NavBarComponent,
-    ContuctUsComponent
+    ContuctUsComponent,
+    AboutUsComponent,
+    AboutPageComponent
   ],
   imports: [
     RouterModule.forRoot(routes),
     BrowserModule,
     AppRoutingModule,
-    NgbModule
+    NgbModule,
+    BrowserAnimationsModule,
+    MatTabsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
